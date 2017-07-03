7ea8b7b62e089381d45dd9abe590be9f8cc1a50c
Vwan <vivian_jh3@hotmail.com>
2017-07-03 21:25:44 +0800

    updated script to output cards by chapters

--- htmlToMd/src/MDConverterViaRest.groovy
+++ htmlToMd/src/MDConverterViaRest.groovy
@@ -4,32 +4,91 @@
 
 import groovy.json.JsonSlurper
 
-url="https://api.github.com/repos/OpenMindClub/OpenMindWorld/issues/42/comments?access_token=9c7d8cd208a4b73747cec766e3948d3f576dd264"
-
-path="/users/wanjia/github.com.vwan/projects/groovy/htmlToMd/resource/"
-src = ["42":"chaoyuezhishang","29":"zhecaishixinlixue"]
-
+date ="2017-07-03"
+src = ["42":"超越智商","29":"这才是心理学"]
+ids=[
+        "1":"一",
+        "2":"二",
+        "3":"三",
+        "4":"四",
+        "5":"五",
+        "6":"六",
+        "7":"七",
+        "8":"八",
+        "9":"九",
+        "10":"十",
+        "11":"十一",
+        "12":"十二",
+        "13":"十三"
+]
+mdfile =""
 src.each{
     key,value->
 
-        newstr = new StringBuilder()
+        path="/users/wanjia/github.com.vwan/projects/OpenMind/cardbook.omw.com/Vwan.omw.com/"+value+"/"
+        new File(path).mkdirs()
+        temp = ""
+        (1..5).each {
+            pageid=it
+            url = "https://api.github.com/repos/OpenMindClub/OpenMindWorld/issues/" + key + "/comments?page=" + pageid + "&access_token="
+println url
 
-        mdfile = path+"issue_"+key+"_"+value+".md"
-    println mdfile
-    content = url.toURL().text
-    println content
-    json = new JsonSlurper().parseText(content)
-    json.each{
-        author = it.user.login
-        println author
-        newstr.append("---")
-        newstr.append("Author:" + author +"\n")
-        body = it.body
-        newstr.append(body+"\n")
+            content=url.toURL()?.text
+            println content
+            if(content) {
+                json = new JsonSlurper().parseText(content)
+                json.each {
+                    author = "\n---\n# 撰写: " + it.user.login + "\n"
+                   // println author
+                    body = it.body
+                    str = replaceChapterNo(author + body, ids)
+                    temp = makeMdFile(str, ids, value)
+                }
+            }
+        }
+        fileWriteOrAppend(new File(path+date+"-TBD-"+value+".md"),temp)
     }
 
-    new File(mdfile).write(newstr.toString())
 
+def replaceChapterNo(str,ids){
+    ids.each{
+        if (str.contains("第"+it.key+"章")) {
+            str = str.replaceAll("第" + it.key + "章", "第" + it.value + "章")
+        }
+        if (str.contains("Chapter"+it.key)) {
+            str = str.replaceAll("Chapter" + it.key, "第" + it.value + "章")
+        }
+    }
+    return str
+}
 
+def makeMdFile(str,ids,bookname){
+    uncatogeried = ""
+    ids.each{
+        if(str.substring(0,40).contains("第"+it.value+"章") ){
+            key = (it.key.toInteger()<10)?"0"+it.key:it.key
+            cfile = new File( path+date+"-"+key+"-"+bookname+".md")
+            fileWriteOrAppend(cfile,str)
+        }
+        isCategoried=false
+        ids.each{
+            if (str.contains("第"+it.value+"章")){
+                isCategoried = true
+                return isCategoried
+            }
+        }
+      //  println isCategoried
+        if (!isCategoried) uncatogeried += str
+
+    }
+
+    return uncatogeried
+}
+
+def fileWriteOrAppend(file,s){
+
+    println s
+    if (!file.exists()) file.write(s)
+    else file.append(s)
 }
 
